2acb1aa76ab2aecfa57a2ea091656648575f246e
Stepan Tramov <tramovs@list.ru>
2025-11-07 17:41:19 +0300

    Добавлена консольная утилита на основе ISBN10

--- src/checksum.py
+++ src/checksum.py
@@ -1,7 +1,6 @@
 def modulo_11_checksum(isbn_number: str):
 
     digits = [int(char) for char in isbn_number if char.isdigit()]
-
     check_digit = digits[-1]
 
     total = 0
@@ -15,3 +14,34 @@ def modulo_11_checksum(isbn_number: str):
 
     return checksum % 11 == 0
 
+def menu():
+    
+    isbn_number = input("Введите номер ISBN: ")
+    
+    if isbn_number == "-1":
+        return
+
+    for i in range(len(isbn_number)):
+        if not isbn_number[i].isdigit() and isbn_number[i] != "-":
+            if i in [1,5,11]:
+                print(f"Ожидалось: -, Введено: {isbn_number[i]}")
+            else:
+                print(f"Ожидалось: цифра, Введено: {isbn_number[i]}")
+            menu()
+
+        if i in [1,5,11] and isbn_number[i] != "-":
+            print(f"Ожидалось: -, Введено: {isbn_number[i]}")
+            menu()
+        
+        if i not in [1,5,11] and isbn_number[i] == "-":
+            print(f"Ожидалось: цифра, Введено: -")
+            menu()
+
+    if len(isbn_number) != 13:
+        print(f"Введено символов: {len(isbn_number)}, Ожидалось: 13")
+        menu()
+
+    print(modulo_11_checksum(isbn_number))
+    menu()
+    
+menu()
