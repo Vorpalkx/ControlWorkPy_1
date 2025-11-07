a2164a2935a30ccd2f7d65f60729ecaa5aed6920
Stepan Tramov <tramovs@list.ru>
2025-11-07 17:09:42 +0300

    Изменена функция checksum в соответствии с приложенной статьей на wiki, также изменены тесты, чтобы проходили

--- src/checksum.py
+++ src/checksum.py
@@ -5,10 +5,13 @@ def modulo_11_checksum(isbn_number: str):
     check_digit = digits[-1]
 
     total = 0
+    weight = 10
     for i in range(len(digits) - 1):
-        weight = 10
         digit = digits[i]
         total += digit * weight
+        weight -= 1
 
     checksum = total + check_digit
+
     return checksum % 11 == 0
+
